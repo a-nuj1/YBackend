be5d3efcc25580a8321d04e7e8fc4c39999b29c6
Anuj <anujgupta1532003@gmail.com>
2024-11-24 10:47:51 +0530

    models done

---
+++ src/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        username:{
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email:{
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        fullname:{
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+
+        avatar:{
+            type: String,
+            required: true
+        },
+
+        coverImage:{
+            type: String,
+        },
+
+        watchHistroy:[
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password:{
+            type: String,
+            required: [true, "Password is required"],
+        },
+
+        refreshToken:{
+            type: String
+        },
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
